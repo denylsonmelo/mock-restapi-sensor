bc6f49f700912651d785f0a6e3632be1a54b1d94
Denylson Melo <denylsonmelo@gmail.com>
2017-02-17 16:33:16 -0300

    mudando tipo de retorno do resource para response e habilitando CORS permissivo

--- src/main/java/br/edu/ifpi/capar/sensor/restapi/mock/resource/DadosResource.java
+++ src/main/java/br/edu/ifpi/capar/sensor/restapi/mock/resource/DadosResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
@@ -13,11 +14,15 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("dados")
 public class DadosResource {
-    
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String retornarExperimento(){
+    public Response retornarExperimento() {
         Random random = new Random();
-        return new Dado(random.nextDouble(), 0, random.nextDouble()).toJson();
+        return Response.ok()
+                .entity(new Dado(random.nextDouble(), 0, random.nextDouble()).toJson())
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET")
+                .allow("OPTIONS").build();
     }
 }
